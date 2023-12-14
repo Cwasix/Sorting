1b7945cf9be5f7958a54e20dab08e566fe7bd80d
Cwasix <145646866+Cwasix@users.noreply.github.com>
2023-12-14 12:58:41 +0300

    Add files via upload

---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите размерность массива");
+int m = Convert.ToInt32 (Console.ReadLine());
+int i, min, swap;
+Console.WriteLine("Введите элементы масива");
+int[] mass = new int[m];
+
+    for (m = 0; m < mass.Length; m++)
+    { 
+    mass[m] = Convert.ToInt32(Console.ReadLine());
+    }
+
+for (i = 1; i < mass.Length - 1; i++)
+{
+    min = mass[i];
+    swap = i;
+    for (m = i + 1; m < mass.Length; m++)
+    {
+        if (m % 2 != 0 || m != 0)
+        {
+            if (mass[m] < min)
+            {
+                min = mass[m];
+                swap = m;
+            }            
+        }    
+    }
+    mass[swap] = mass[i];
+    mass[i] = min;
+}
+
+Console.WriteLine( "Вывод масива" );
+for (m = 0; m < mass.Length; m++)
+{
+   Console.WriteLine($"{mass[m]}");
+}
